b6a9fae7704698c99fcae6a46e0503e3b63b6d02
Eran Kampf <eran@ekampf.com>
2015-07-16 18:12:52 -0700

    Easier bootstrap

--- bootstrap.rb
+++ bootstrap.rb
@@ -8,16 +8,29 @@ require 'inquirer'
 
 DEFAULTS = File.exist?("defaults.yml") ? YAML::load_file("defaults.yml") : {}
 
+def run(cmd)
+  puts "[Running] #{cmd}"
+  `#{cmd}` unless ENV['DEBUG']
+end
+
 def install
+    puts "======================================================"
+    puts "Setting up ZSH"
+    puts "======================================================"
     install_oh_my_zsh()
     switch_to_zsh()
-    
+
+    puts "======================================================"
+    puts "Symlinking files"
+    puts "======================================================"    
     files = get_files_to_process()
     puts "Processing:\n "
     files.each { |f| puts "\t#{f}" }
     puts
     puts
     
+    file_options = %w{yes no always}
+    always_overwrite = false
     files.each do |src_filename|
         f = src_filename.split("/", 2)[1] # Get rid of the "dotfiles/" prefix        
         src_fullname = File.absolute_path(src_filename)
@@ -31,7 +44,15 @@ def install
             if File.identical?(f, target_filename)
                 puts "\tfiles are identical"
             else
-                should_overwrite = Ask.confirm("File already exists: #{target_filename}. Overwrite it?", clear: true, response: false, default: false)
+                # should_overwrite = Ask.confirm("File already exists: #{target_filename}. Overwrite it?", clear: true, response: false, default: false)
+                
+                should_overwrite = always_overwrite
+                if !always_overwrite
+                    response = Ask.list("File already exists: #{target_filename}. Overwrite it?", file_options)
+                    always_overwrite = true if (file_options[response].to_sym == :always)
+                    should_overwrite = always_overwrite or response == 1
+                end
+
                 if should_overwrite
                     puts "\tRemoving #{target_filename}"
                     FileUtils.rm(target_filename)
@@ -47,7 +68,7 @@ def install
 end
 
 def get_files_to_process
-    files = Dir.glob("dotfiles/**/*") - %w[bootstrap.rb defaults.yml README.md LICENSE oh-my-zsh .gitignore]
+    files = Dir.glob("dotfiles/**/*") - %w[bootstrap.rb defaults.yml README.md LICENSE oh-my-zsh .gitignore ]
     files.reject! { |f| File.directory?(f) }
     files
 end
@@ -107,6 +128,29 @@ def install_oh_my_zsh
         puts "installing oh-my-zsh"
         system %Q{git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh"}
     end
+end
+
+def iTerm_available_themes
+   Dir['iTerm2/*.itermcolors'].map { |value| File.basename(value, '.itermcolors')} << 'None'
+end
+
+def iTerm_profile_list
+  profiles=Array.new
+  begin
+    profiles <<  %x{ /usr/libexec/PlistBuddy -c "Print :'New Bookmarks':#{profiles.size}:Name" ~/Library/Preferences/com.googlecode.iterm2.plist 2>/dev/null}
+  end while $?.exitstatus==0
+  profiles.pop
+  profiles
+end
+
+def apply_theme_to_iterm_profile_idx(index, color_scheme_path)
+  values = Array.new
+  16.times { |i| values << "Ansi #{i} Color" }
+  values << ['Background Color', 'Bold Color', 'Cursor Color', 'Cursor Text Color', 'Foreground Color', 'Selected Text Color', 'Selection Color']
+  values.flatten.each { |entry| run %{ /usr/libexec/PlistBuddy -c "Delete :'New Bookmarks':#{index}:'#{entry}'" ~/Library/Preferences/com.googlecode.iterm2.plist } }
+
+  run %{ /usr/libexec/PlistBuddy -c "Merge '#{color_scheme_path}' :'New Bookmarks':#{index}" ~/Library/Preferences/com.googlecode.iterm2.plist }
+  run %{ defaults read com.googlecode.iterm2 }
 end
 
 install
